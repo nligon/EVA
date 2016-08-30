08e9532ff9b9c12e9daa707bd3c29e3bfbef8b84
Nathanael Ligon <nathanaelligon@gmail.com>
2016-08-29 23:10:46 -0700

    Minor modifications.

--- client/app/view1/view1.js
+++ client/app/view1/view1.js
@@ -16,6 +16,14 @@ angular.module('talkie.view1', [])
       });
   }
 
+  $scope.fetch = function() {
+    $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+      .then(function(response) {
+        $scope.details = response.data;
+        console.log('response:', response.data);
+      });
+  }
+
   $scope.select = function() {
     this.setSelectionRange(0, this.value.length);
   }

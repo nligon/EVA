93f8458264f56526bfbe76c0ba989286863fd1f3
Nathanael Ligon <nathanaelligon@gmail.com>
2016-08-29 22:54:35 -0700

    Implement API calls to test movie database, implement auto-rendering of specific response properties every 800ms.

--- client/app/view1/view1.js
+++ client/app/view1/view1.js
@@ -1,5 +1,23 @@
+// Movie Controller
 angular.module('talkie.view1', [])
 
-.controller('view1Ctrl', function($scope) {
-  $scope.display = 'First view, here.';
+.controller('view1Ctrl', function($scope, $http) {
+  // $scope.display = 'First view, here.';
+
+  $scope.$watch('search', function() {
+    fetch();
+  });
+
+  function fetch() {
+    $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+      .then(function(response) {
+        $scope.details = response.data;
+        console.log('response:', response.data);
+      });
+  }
+
+  $scope.select = function() {
+    this.setSelectionRange(0, this.value.length);
+  }
+
 })
